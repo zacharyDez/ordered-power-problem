03f9243866740748a609cf1b82a5521663ca7cb8
dezz2201 <zachary.deziel@usherbrooke.ca>
2020-12-28 23:45:55 -0500

    completed second algorithm and added bechmarks

--- src/power_ordered.go
+++ src/power_ordered.go
@@ -23,3 +23,82 @@ func GetPowerOrderedStartWithLimits(l []int) []int {
 
 	return res
 }
+
+func addPowerTwo(dst []int, src []int, idx int) []int {
+	dst = append(dst, int(math.Pow(float64(src[idx]), 2.0)))
+	return dst
+}
+
+// GetPowerOrderedStartWithZero takes an ordered list as input and returns an ordered list of the elements to the power of 2.
+// It is assumed that the list passed in ordered.
+func GetPowerOrderedStartWithZero(l []int) []int {
+	initLen := len(l)
+	var res []int
+
+	start := findAbsoluteSmallestIdx(l)
+	right := start + 1
+	left := start - 1
+
+	res = addPowerTwo(res, l, start)
+	for i := 0; i < initLen-1; i++ {
+		if left < 0 {
+			res = addPowerTwo(res, l, right)
+			right++
+			continue
+		}
+
+		if right > initLen-1 {
+			res = addPowerTwo(res, l, left)
+			left--
+			continue
+		}
+
+		if math.Abs(float64(l[right])) < math.Abs(float64(l[left])) {
+			res = addPowerTwo(res, l, right)
+			right++
+		} else {
+			res = addPowerTwo(res, l, left)
+			left--
+		}
+	}
+	return res
+}
+
+func findAbsoluteSmallestIdx(l []int) int {
+	for i := 0; ; i++ {
+		posIdx := binarySearch(l, i)
+		negIdx := binarySearch(l, -i)
+		if posIdx != -1 {
+			return posIdx
+		}
+
+		if negIdx != -1 {
+			return negIdx
+		}
+	}
+}
+
+func binarySearch(array []int, target int) int {
+	startIndex := 0
+	endIndex := len(array) - 1
+	midIndex := len(array) / 2
+
+	for startIndex <= endIndex {
+		value := array[midIndex]
+
+		if value == target {
+			return midIndex
+		}
+
+		if value > target {
+			endIndex = midIndex - 1
+			midIndex = (startIndex + endIndex) / 2
+			continue
+		}
+
+		startIndex = midIndex + 1
+		midIndex = (startIndex + endIndex) / 2
+	}
+
+	return -1
+}
--- src/power_ordered_test.go
+++ src/power_ordered_test.go
@@ -10,3 +10,49 @@ func TestPowerOrderedStartWithLimits(t *testing.T) {
 	l := []int{-10, -3, -2, 0, 1, 2, 3, 10}
 	assert.EqualValues(t, []int{0, 1, 4, 4, 9, 9, 100, 100}, GetPowerOrderedStartWithLimits(l))
 }
+
+func TestPowerOrderedStartWithZero(t *testing.T) {
+	l := []int{-10, -3, -2, 0, 1, 2, 3, 10}
+	assert.EqualValues(t, []int{0, 1, 4, 4, 9, 9, 100, 100}, GetPowerOrderedStartWithZero(l))
+}
+
+func getElements(n int) []int {
+	result := make([]int, n)
+	i := 0
+	sign := -1
+	for j := n - 1; j >= 0; j-- {
+		result[i] = j * sign
+		i++
+		sign = sign * -1
+	}
+	return result
+}
+
+func BenchmarkPowerOrderedStartWithZeroSingleZero(b *testing.B) {
+	els := []int{0}
+	GetPowerOrderedStartWithZero(els)
+}
+
+func BenchmarkPowerOrderedStartWithLimitsSingleZero(b *testing.B) {
+	els := []int{0}
+	GetPowerOrderedStartWithLimits(els)
+}
+
+func BenchmarkPowerOrderedStartWithZeroFiveEltsMillionTimesMillion(b *testing.B) {
+	els := []int{100000000}
+	GetPowerOrderedStartWithZero(els)
+}
+
+func BenchmarkPowerOrderedStartWithLimitsFiveEltsMillionTimesMillion(b *testing.B) {
+	els := []int{100000000}
+	GetPowerOrderedStartWithLimits(els)
+}
+func BenchmarkPowerOrderedStartWithZeroSimpleList(b *testing.B) {
+	els := []int{-10, -3, -2, 0, 1, 2, 3, 10}
+	GetPowerOrderedStartWithZero(els)
+}
+
+func BenchmarkPowerOrderedStartWithLimitsSimpleList(b *testing.B) {
+	els := []int{-10, -3, -2, 0, 1, 2, 3, 10}
+	GetPowerOrderedStartWithLimits(els)
+}
